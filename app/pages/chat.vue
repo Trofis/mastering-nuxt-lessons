543bb9be64069ac1e614d082d29c11de26b08fb9
trofis <porto.thomas@gmail.com>
2026-01-29 23:10:38 +0100

    feat: useHeadSafe

--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -2,6 +2,21 @@
 import useChat from "~/composables/useChat";
 
 const { chat, messages, sendMessage } = useChat();
+
+// Change settings of the page
+useHeadSafe({
+  title: chat.value?.title,
+  htmlAttrs: {
+    lang: "en",
+    class: "my-reality-is-cool",
+  },
+  // Safe prevents injecting script
+  // script: [
+  //   {
+  //     innerHTML: "window.analytics.track('page_view', { page: 'chat' })",
+  //   },
+  // ],
+});
 </script>
 
 <template>
