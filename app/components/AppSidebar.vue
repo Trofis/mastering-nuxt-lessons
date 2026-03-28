44691ea81723a8b9f3bcdca29ae0ff2577ee1a19
trofis <porto.thomas@gmail.com>
2026-03-28 17:15:31 +0100

    feat: appSideBar group by dates + dateUtils

--- app/components/AppSidebar.vue
+++ app/components/AppSidebar.vue
@@ -2,6 +2,7 @@
 import useChats from "~/composables/useChats";
 import type { Chat } from "~/types";
 import type { NavigationMenuItem } from "@nuxt/ui";
+import { filterChatsByDateRange } from "~/utils/dateUtils";
 
 defineProps<{
   isOpen: boolean;
@@ -21,6 +22,25 @@ const formatChatItem = (chat: Chat): NavigationMenuItem => {
     defaultOpen: true,
   };
 };
+
+const chatsWithoutProject = computed(() =>
+  chats.value.filter((c) => c.projectId === undefined),
+);
+
+const filterChats = (startDays: number, endDays?: number) => {
+  return computed(() => {
+    return filterChatsByDateRange(
+      chatsWithoutProject.value,
+      startDays,
+      endDays,
+    ).map(formatChatItem);
+  });
+};
+
+const todayChats = filterChats(-1, 1);
+const lastWeekChats = filterChats(1, 7);
+const lastMonthChats = filterChats(7, 30);
+const olderChats = filterChats(30);
 
 const formattedChats = computed(() => chats.value.map(formatChatItem));
 
@@ -42,40 +62,84 @@ const handleNewChat = async () => {
     class="fixed left-0 top-16 bottom-0 z-50 w-72 border-r border-[var(--ui-border)] bg-[var(--ui-bg-elevated)] transition-transform duration-200 ease-in-out"
     :class="isOpen ? 'translate-x-0' : '-translate-x-full'"
   >
-    <div class="flex h-full flex-col">
+    <div class="flex h-full justify-between flex-col">
       <div
+        v-if="
+          todayChats.length > 0 ||
+          lastWeekChats.length > 0 ||
+          lastMonthChats.length > 0 ||
+          olderChats.length > 0
+        "
         class="flex items-center justify-between border-b border-[var(--ui-border)] p-4"
       >
-        <h2 class="text-lg font-semibold text-[var(--ui-text)]">Chats</h2>
-        <UButton
+        <div v-if="todayChats.length > 0">
+          <h2 class="text-lg font-semibold text-[var(--ui-text)]">Today</h2>
+          <!-- <UButton
           icon="i-lucide-x"
           color="neutral"
           variant="soft"
           class="lg:hidden"
           @click="$emit('toggle-sidebar')"
+        /> -->
+          <UNavigationMenu
+            class="w-full mb-4 px-1.5"
+            orientation="vertical"
+            :items="todayChats"
+            default-open
+          />
+        </div>
+
+        <div
+          v-if="lastWeekChats.length > 0"
+          class="flex items-center justify-between border-b border-[var(--ui-border)] p-4"
+        >
+          <h2 class="text-lg font-semibold text-[var(--ui-text)]">Last Week</h2>
+          <UNavigationMenu
+            class="w-full mb-4 px-1.5"
+            orientation="vertical"
+            :items="lastWeekChats"
+            default-open
+          />
+        </div>
+
+        <div
+          v-if="lastMonthChats.length > 0"
+          class="flex items-center justify-between border-b border-[var(--ui-border)] p-4"
+        >
+          <h2 class="text-lg font-semibold text-[var(--ui-text)]">
+            Last Month
+          </h2>
+          <UNavigationMenu
+            class="w-full mb-4 px-1.5"
+            orientation="vertical"
+            :items="lastMonthChats"
+            default-open
+          />
+        </div>
+
+        <div
+          v-if="olderChats.length > 0"
+          class="flex items-center justify-between border-b border-[var(--ui-border)] p-4"
+        >
+          <h2 class="text-lg font-semibold text-[var(--ui-text)]">Older</h2>
+          <UNavigationMenu
+            class="w-full mb-4 px-1.5"
+            orientation="vertical"
+            :items="olderChats"
+            default-open
+          />
+        </div>
+      </div>
+
+      <div v-else class="overflow-y-auto p-4">
+        <UAlert
+          title="No chats"
+          description="Start by creating a new chat"
+          color="neutral"
+          variant="soft"
+          class="mt-2"
         />
       </div>
-      <UNavigationMenu
-        class="w-full mb-4 px-1.5"
-        orientation="vertical"
-        :items="formattedChats"
-        default-open
-      />
-      <!-- <div class="flex-1 overflow-y-auto p-2">
-        <NuxtLink
-          v-for="chat in chats"
-          :key="chat.id"
-          :to="`/chats/${chat.id}`"
-          class="mb-1 block rounded-lg px-3 py-2 text-sm transition-colors hover:bg-[var(--ui-bg-muted)] cursor-pointer"
-          :class="[
-            route.params.id === chat.id
-              ? 'bg-[var(--ui-bg-muted)] text-[var(--ui-text)] font-medium'
-              : 'text-[var(--ui-text-muted)]',
-          ]"
-        >
-          {{ chat.title || "New Chat" }}
-        </NuxtLink>
-      </div> -->
 
       <div class="border-t border-[var(--ui-border)] p-4">
         <UButton
